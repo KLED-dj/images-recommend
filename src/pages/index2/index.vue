f8ccd15a7cc384c079bd75f6ceca3e6d3a64af43
KLED_dj <476089856@qq.com>
2023-02-16 14:08:15 +0800

    Changes to be committed: 	new file:   package-lock.json 	modified:   package.json 	modified:   project.config.json 	modified:   project.private.config.json 	modified:   src/pages.json 	new file:   src/pages/index2/index.vue 	new file:   src/pages/index3/index.vue 	modified:   yarn.lock

---
+++ src/pages/index2/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		<view class="rpx">rpx</view>
+		<view class="vw">vw</view>
+		<view class="sass">sass</view>
+</view>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.rpx {
+	width: 750rpx;
+	height: 100px;
+	background-color: cyan;
+}
+
+.vw {
+	width: 50vw;
+	height: 50vw;
+	background-color: tan;
+}
+
+.content {
+	.sass {
+		background-color: $uni-color-title;
+		color: $uni-color-error
+	}
+}
+</style>
---
+++ src/pages/index3/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<view>{{ msg }}</view>
+		<view>{{ price }}</view>
+		<view>{{ isRich }}</view>
+		<view>{{ person.name }}</view>
+		<view>{{ person.skill }}</view>
+		<!-- 在标签上 通过属性的方式使用数据-->
+		<view v-bind:data-color="color">color</view>
+
+		<view>============================</view>
+		<view v-for="( item ) in list" :key="item.id">
+			{{ item.text }}--{{ item.id }}
+		</view>
+</view>
+</template>
+<script>
+
+
+export default {
+	data() {
+		return {
+			msg: "nihao",
+			price: 1000,
+			isRich: false,
+			person: {
+				name: "悟空",
+				skill: "大闹天宫"
+			},
+			color: "red",
+			list: [
+				{
+					id: 0,
+					text: "苹果"
+				},
+				{
+					id: 1,
+					text: "香蕉"
+				},
+				{
+					id: 2,
+					text: "梨"
+				}
+			]
+		}
+	}
+}
+</script>
+
+<style lang="scss"></style>
